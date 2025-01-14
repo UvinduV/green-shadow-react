b42615d2b630b19f2ead89eb3c2bb2dd3ac097ee
UvinduV <uvindusanketh470@gmail.com>
2025-01-14 17:36:40 +0530

    add tellwined

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,18 +1,20 @@
 import {Link} from "react-router";
+import "./Navigation.css";
+
 
 export function Navigation() {
     return (
         <>
-            <header>
-                <nav>
-                    <ul>
-                        <Link to="/">Dashboard</Link>
-                        <Link to="/field">Field</Link>
-                        <Link to="/crop">Crop</Link>
-                        <Link to="/staff">Staff</Link>
-                        <Link to="/vehicle">Vehicle</Link>
-                        <Link to="/equipment">Equipment</Link>
-                        <Link to="/logs">Logs</Link>
+            <header className="header-main">
+                <nav className="px-4 py-3">
+                    <ul className="flex text-white space-x-4">
+                        <Link to="/" className="custom-link border-2">Dashboard</Link>
+                        <Link to="/field" className="custom-link">Field</Link>
+                        <Link to="/crop" className="custom-link">Crop</Link>
+                        <Link to="/staff" className="custom-link">Staff</Link>
+                        <Link to="/vehicle" className="custom-link">Vehicle</Link>
+                        <Link to="/equipment" className="custom-link">Equipment</Link>
+                        <Link to="/logs" className="custom-link">Logs</Link>
                     </ul>
                 </nav>
             </header>
